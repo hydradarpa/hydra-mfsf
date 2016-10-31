1390f27f8f3f750e55e4835ae83bcfde0a0a988a
Ben Lansdell <lansdell@uw.edu>
2016-10-31 16:27:02 -0700

    Add deep flow correspondence analysis

---
+++ deepflow_corrmatrix.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+import sys 
+import argparse 
+import os.path 
+import os
+
+import numpy as np 
+from glob import glob 
+
+from cvtools import readFlo
+
+import matplotlib.pyplot as plt
+import cv2 
+
+def main():
+	usage = """deepflow_corrmatrix.py [register_dir]...
+
+Generate confidence maps based on optical flow estimation
+
+Example: 
+./deepflow_corrmatrix.py ./register/20160412stk0001/
+
+For help:
+./deepflow_corrmatrix.py -h 
+
+Ben Lansdell
+10/30/2016
+"""
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('dir_in', help='output directory')
+	args = parser.parse_args()
+
+	#Test code for interactive dev
+	class Args:
+		pass 
+	args = Args()
+	args.dir_in = './register/20160412stk0001/'
+
+	#Get the set of reference frames...
+	refframes = [int(i[-8:-4]) for i in glob(args.dir_in + 'refframes/*.png')]
+	nF = len(refframes)
+
+	threshold = 4
+
+	#Load DeepFlow results
+	for r1 in refframes:
+		for r2 in refframes:
+			if r1 != r2:
+
+				print("Load DeepFlow between frame %d and %d" %(r1, r2))
+				fn_in1 = args.dir_in + 'corrmatrix/%04d_%04d.flo'%(r1, r2)
+				fn_in2 = args.dir_in + 'corrmatrix/%04d_%04d.flo'%(r2, r1)
+
+				flow1 = readFlo(fn_in1)
+				flow2 = readFlo(fn_in2)
+
+				nx = flow1.shape[0]
+				ny = flow1.shape[1]
+
+				#Flip x and y flow
+				flow1 = np.transpose(flow1, [1,0,2])
+				flow2 = np.transpose(flow2, [1,0,2])
+				flow1 = flow1[:,:,::-1]
+				flow2 = flow2[:,:,::-1]
+
+				#Perform mapping and then reverse mapping, then perform reverse mapping then mapping
+				#Make mesh grid
+				fwdmeshx, fwdmeshy = [a.astype(np.float32) for a in np.meshgrid(np.arange(nx), np.arange(ny))]
+				revmeshx, revmeshy = [a.astype(np.float32) for a in np.meshgrid(np.arange(nx), np.arange(ny))]
+
+				#Perturb mesh grid by forward flow 
+				#Round to integers 
+				fwdx = fwdmeshx + np.ceil(flow1[:,:,0])
+				fwdy = fwdmeshy + np.ceil(flow1[:,:,1])
+
+				fwdx = np.maximum(0, np.minimum(nx-1, fwdx))
+				fwdy = np.maximum(0, np.minimum(nx-1, fwdy))
+
+				#Look up flow field using this perturbed map
+				fwdremapx = fwdx + flow2[fwdx.astype(int),fwdy.astype(int),0]
+				fwdremapy = fwdy + flow2[fwdx.astype(int),fwdy.astype(int),1]
+
+				fwdremapx -= fwdmeshx 
+				fwdremapy -= fwdmeshy 
+
+				fwderr = np.sqrt(fwdremapx**2 + fwdremapy**2)
+
+				#Determine based on absolute value of displacement whether 'mapped' or not...
+				#perhaps model as GMM/some sort of smoothing....
+				#plt.pcolor(fwderr)
+				#plt.colorbar()
+				#plt.show()
+
+				#Plot flow data 
+				#absflow1 = np.sqrt(flow1[:,:,0]**2 + flow1[:,:,1]**2)
+				#absflow2 = np.sqrt(flow2[:,:,0]**2 + flow2[:,:,1]**2)
+
+				#Load reference frame
+				fn_in1 = args.dir_in + 'refframes/frame_%04d.png'%r1
+				rf1 = cv2.imread(fn_in1)
+				fn_in2 = args.dir_in + 'refframes/frame_%04d.png'%r2
+				rf2 = cv2.imread(fn_in2)
+
+				hsv = np.zeros_like(rf1)
+				hsv[...,1] = 255
+				mag, ang = cv2.cartToPolar(flow1[...,0], flow1[...,1])
+				hsv[...,0] = ang*180/np.pi/2
+				hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+				bgr1 = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+				dst1 = cv2.addWeighted(rf1,0.7,bgr1,0.3,0)
+				#cv2.imshow('f',dst1)
+
+				hsv = np.zeros_like(rf2)
+				hsv[...,1] = 255
+				mag, ang = cv2.cartToPolar(flow2[...,0], flow2[...,1])
+				hsv[...,0] = ang*180/np.pi/2
+				hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+				bgr2 = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+				dst2 = cv2.addWeighted(rf2,0.7,bgr2,0.3,0)
+				#cv2.imshow('f',dst2)
+
+				#Overlay with reference frame...
+				im_fwderr = cv2.normalize(fwderr, im_fwderr, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+				im_fwderr = cv2.cvtColor(im_fwderr, cv2.COLOR_GRAY2BGR)
+				im_fwderr = cv2.applyColorMap(im_fwderr, cv2.COLORMAP_JET)
+				dst = cv2.addWeighted(rf1,0.7,im_fwderr,0.3,0)
+
+				#cv2.imshow('Error', dst)
+
+				#Concat into one big image...
+				vis1 = np.concatenate((rf1, rf2, bgr1), axis=1)
+				vis2 = np.concatenate((rf2, rf1, bgr2), axis=1)
+				vis3 = np.concatenate((bgr1, bgr2, im_fwderr), axis=1)
+				vis = np.concatenate((vis1, vis2, vis3), axis = 0)
+				cv2.imshow('Error', vis)
+
+				#Other measures of error include just comparing the flow mapped R1 to R2
+				#and vice versa
+				#Can use CV's remap function
+
+				rf1_recon = cv2.remap(rf2, flow1[:,:,0], flow1[:,:,1], cv2.INTER_LINEAR)
+				rf2_recon = cv2.remap(rf1, flow2[:,:,0], flow2[:,:,1], cv2.INTER_LINEAR)
+
+				#Compare these reconstructions to the original frames....
+				vis1 = np.concatenate((rf1, rf1_recon), axis = 1)
+				vis2 = np.concatenate((rf2, rf2_recon), axis = 1)
+				vis = np.concatenate((vis1, vis2), axis = 0)
+				cv2.imshow('Recon', vis)
+
+				#Run local comparisons within a window...
+				sim1 = np.zeros((nx,ny))
+				sim2 = np.zeros((nx,ny))
+				for i in range(nx):
+					for j in range(ny):
+						#Generate window around current point
+
+						#Measure similiarty here
+
+				#Save these results
+
+
+				os.system(DF + ' %s %s %s -match %s' %(im2, im1, fn_out, matches)) 
+
+if __name__ == "__main__":
+	sys.exit(main())
---
+++ deepflow_refframes.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import sys 
+import argparse 
+import os.path 
+import os
+
+import numpy as np 
+import libtiff as lt 
+from libtiff import TIFF
+
+DM = '~/deepmatching/deep_matching_gpu.py'
+DF = '~/deepflow/deepflow2-static'
+
+def main():
+	usage = """deepflow_refframes.py [input_tiff] [output_dir] [ref frame 1] [ref frame 2] <ref frame 3> ...
+
+Normalize intensity of tiff stack 
+
+Example: 
+./deepflow_refframes.py "../hydra/video/20160412/stk_0001_Substack (1-5000).tif" ./register/20160412stk0001/ 1 40 80 120 
+
+For help:
+./deepflow_refframes.py -h 
+
+Ben Lansdell
+10/27/2016
+"""
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('fn_in', help='input tiff file')
+	parser.add_argument('dir_out', help='output directory')
+	parser.add_argument('ref_frames', help='reference frames', nargs = '+', type = int)
+	args = parser.parse_args()
+
+	#Test code
+	class Args:
+		pass 
+	args = Args()
+	args.fn_in = '../hydra/video/20160412/stk_0001_Substack (1-5000).tif'
+	args.ref_frames = [1, 40, 80, 120, 160]
+	args.dir_out = './register/20160412stk0001/'
+
+	#args.ref_frames = [1, 2, 3]
+	#args.dir_out = './register/test/'
+
+	nF = len(args.ref_frames)
+	images = []
+
+	#Make video directory
+	if not os.path.isdir(args.dir_out + 'refframes'):
+		os.makedirs(args.dir_out + 'refframes')
+	if not os.path.isdir(args.dir_out + 'corrmatrix'):
+		os.makedirs(args.dir_out + 'corrmatrix')
+	
+	#Use convert to extract frames
+
+	# to open a tiff file for reading:
+	#tif = TIFF.open(args.fn_in, mode='r')
+	#for idx, image in enumerate(tif.iter_images()): # do stuff with image
+	#	if idx in args.ref_frames:
+	#		images.append(image)
+	#tif.close()
+
+	for frame in args.ref_frames:
+		os.system('convert "%s"[%d] -resize 50%% %s/refframes/frame_%04d.tif'%(args.fn_in, frame, args.dir_out, frame))
+		os.system('convert "%s"[%d] -resize 50%% -auto-level -depth 8 %s/refframes/frame_%04d.png'%(args.fn_in, frame, args.dir_out, frame))
+
+	#for frame, image in zip(args.ref_frames, images):
+	#	fn_out = args.dir_out + 'refframes/frame_%04d.tif'%frame
+	#	tif_out = TIFF.open(fn_out, mode='w')
+	#	tif_out.write_image(image)
+	#	tif_out.close()
+
+	#Run DeepMatching
+	for i in range(nF):
+		im1 = args.dir_out + 'refframes/frame_%04d.tif'%args.ref_frames[i]
+		for j in range(nF):
+			if i != j:
+				print("DeepMatching between frame %d and %d" %(args.ref_frames[i], args.ref_frames[j]))
+				im2 = args.dir_out + 'refframes/frame_%04d.tif'%args.ref_frames[j]
+				fn_out = args.dir_out + 'corrmatrix/%04d_%04d.txt'%(args.ref_frames[i],args.ref_frames[j])
+				os.system('python %s %s %s -out %s' %(DM, im1, im2, fn_out)) 
+
+	#Run DeepFlow
+	for i in range(nF):
+		im1 = args.dir_out + 'refframes/frame_%04d.png'%args.ref_frames[i]
+		for j in range(nF):
+			if i != j:
+				print("DeepFlow between frame %d and %d" %(args.ref_frames[i], args.ref_frames[j]))
+				im2 = args.dir_out + 'refframes/frame_%04d.png'%args.ref_frames[j]
+				fn_out = args.dir_out + 'corrmatrix/%04d_%04d.flo'%(args.ref_frames[i],args.ref_frames[j])
+				matches = args.dir_out + 'corrmatrix/%04d_%04d.txt'%(args.ref_frames[i],args.ref_frames[j])
+				os.system(DF + ' %s %s %s -match %s' %(im1, im2, fn_out, matches)) 
+				fn_out = args.dir_out + 'corrmatrix/%04d_%04d.flo'%(args.ref_frames[j],args.ref_frames[i])
+				matches = args.dir_out + 'corrmatrix/%04d_%04d.txt'%(args.ref_frames[j],args.ref_frames[i])
+				os.system(DF + ' %s %s %s -match %s' %(im2, im1, fn_out, matches)) 
+
+if __name__ == "__main__":
+	sys.exit(main())
