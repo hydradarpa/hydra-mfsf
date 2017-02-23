52ecae671d5caec53ad4ffccdbd674d2716cc413
Ben Lansdell <lansdell@uw.edu>
2017-02-23 15:45:19 -0800

    Closes #16. Times icy particle tracker

--- icy_particletracker.py
+++ icy_particletracker.py
@@ -11,6 +11,7 @@ from xlrd import open_workbook
 import cPickle as pickle
 
 import numbers 
+import timeit
 
 def parseSheet(sheet):
 	tracks = []
@@ -33,7 +34,7 @@ def parseSheet(sheet):
 	return tracks 
 
 def main():
-	usage = """icy_particletracker.py [frames_in] [fn_out] -maxfiles MAXFILES
+	usage = """icy_particletracker.py [frames_in] [fn_out] -maxfiles MAXFILES -runtime RUNTIME
 
 	Run particle tracking method on series of tif files using bioimage analysis
 	program Icy. Calls program at the command line. 
@@ -49,12 +50,19 @@ def main():
 	parser.add_argument('frames_in', help='path to .tif files, will use all tif files in folder')
 	parser.add_argument('fn_out', help='output filename for python dictionary of tracks')
 	parser.add_argument('-maxfiles', help='maximum number of tif files to read', default=250)
+	parser.add_argument('-runtime', help='if provided will store time in seconds Icy took to run', default=None)
 	args = parser.parse_args()
+
+	runtime = args.runtime
+	mf = int(args.maxfiles)
 	fn_out = args.fn_out
 	frames_in = args.frames_in
 	fin = [abspath(p) for p in sorted(glob(frames_in + '*.tif'))]
 	fin = fin[0:mf]
 	frames = ':'.join(fin)
+
+	xls_in = frames.split(':')[0] + '_tracking.xls'
+	os.system('rm %s'%xls_in)
 
 	#Edit .xml protocol with input tif files
 	tree = ET.parse(icypath + protocol)
@@ -63,17 +71,27 @@ def main():
 	infiles.set('value', frames)
 	tree.write(icypath + protocol)
 
-	print 'Running Icy particle tracking'
+	print '\n=============================='
+	print 'Running Icy particle tracking:'
+	print '==============================\n'
+
+	starttime = timeit.default_timer()
 	cmd = 'cd %s; java -jar icy.jar --headless --execute plugins.adufour.protocols.Protocols protocol=%s/%s'%(icypath, icypath, protocol)
 	os.system(cmd)
-	print '...done.'
+	elapsed = timeit.default_timer() - starttime
+
+	print '\n=============================='
+	print '...done in %f seconds.'%elapsed
 
 	print 'Saving results to python dictionary'
-	xls_in = frames.split(':')[0] + '_tracking.xls'
 	wb = open_workbook(xls_in)
 	sheet = wb.sheet_by_name('Tracks')
 	tracks = parseSheet(sheet)
 	pickle.dump(tracks, open(fn_out, 'wb'))
+
+	if runtime:
+		with open(runtime, 'a') as fh:
+			fh.write('%d,%f\n'%(mf, elapsed))
 
 if __name__ == "__main__":
 	sys.exit(main())
